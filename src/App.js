46d367b4d481d4b6fe8f785c533f50c1087534c2
parthgupta118 <guptaparth118@gmail.com>
2022-04-14 17:33:09 +0530

    Added Redux Thunk and Toast Container

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import React, { Fragment, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
 
-import Home from "./pages/Home";
-import Signup from "./pages/Signup";
-import "./App.css";
+// Imported Components for rendering pages
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/route/PrivateRoute";
+import Signup from "./pages/SignUp";
 import Login from "./pages/Login";
 
-import Navbar from "./components/Navigation/Navbar";
-import DetailPostCard from "./pages/DetailPostCard";
-import Profile from "./pages/Profile";
-import EditProfile from "./pages/EditProfile";
-import CreatePost from "./pages/CreatePost";
-import PrivateRoute from "./components/Route/PrivateRoute";
-import { ToastContainer } from "react-toastify";
-import { authActions, loadUser } from "./slices/authSlice";
+// Imported Redux Thunk Actions from slices
+import { authActions } from "./slices/authSlice";
+import { toDoActions } from "./slices/toDoSlice";
+
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const token = useSelector((state) => state.auth.token);
-  const isAuth = useSelector((state) => state.auth.isAuthenticated);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // check for token in LS when app first runs
-    // if (localStorage.token) {
-    //   // if there is a token set axios headers for all requests
-    //   setAuthToken(localStorage.token);
-    // }
-    // try to fetch a user, if no token or invalid token we
-    // will get a 401 response from our API
-    // dispatch(loadUser());
-
-    // log user out from all tabs if they log out in one tab
-    // window.addEventListener("storage", () => {
-    //   if (!localStorage.token) dispatch(authActions.logoutUser());
-    // });
-  }, []);
 
   return (
     <Fragment>
-      <ToastContainer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="colored"
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
 
       <Routes>
         {/* Unauthorized Routes */}
@@ -47,23 +39,7 @@ function App() {
         <Route path="login" element={<Login />} />
 
         {/* Protected Routes */}
-        <Route path="/" element={<PrivateRoute component={Home} />} />
-        <Route
-          path="post"
-          element={<PrivateRoute component={DetailPostCard} />}
-        />
-
-        <Route path="profile" element={<PrivateRoute component={Profile} />} />
-
-        <Route
-          path="edit-profile"
-          element={<PrivateRoute component={EditProfile} />}
-        />
-
-        <Route
-          path="create-post"
-          element={<PrivateRoute component={CreatePost} />}
-        />
+        <Route path="/" element={<PrivateRoute component={Dashboard} />} />
       </Routes>
     </Fragment>
   );
